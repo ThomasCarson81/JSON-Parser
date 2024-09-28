06d90dd41c1680a6ab340e194f524592b794bfc5
Thomas Carson <thomas.carson2024@gmail.com>
2024-09-28 19:00:11 +0100

    Added tokenization of brackets and quotes
    Single and double quotes are handled separately, meaning one cannot be used inside the other without causing errors.

--- Program.cs
+++ Program.cs
@@ -35,7 +35,96 @@ namespace JsonParser
         public static List<Token> Tokenize(string json)
         {
 
-        }
+            List<Token> tokens = new List<Token>();
 
+            int totalOpenCurlies = 0;
+            int totalCloseCurlies = 0;
+            int totalOpenSquares = 0;
+            int totalCloseSquares = 0;
+            int totalOpenSingleQuotes = 0;
+            int totalCloseSingleQuotes = 0;
+            int totalOpenDoubleQuotes = 0;
+            int totalCloseDoubleQuotes = 0;
+
+            int currentCurlies = 0;
+            int currentSquares = 0;
+            bool currentSingleQuotes = false;
+            bool currentDoubleQuotes = false;
+
+            bool ignore = false;
+
+            foreach (char c in json)
+            {
+                if (ignore)
+                {
+                    tokens.Add(Token.CHAR);
+                    ignore = false;
+                    continue;
+                }
+                if (c == '\\')
+                {
+                    ignore = true;
+                    continue;
+                }
+                if (c == '"')
+                {
+                    if (currentDoubleQuotes)
+                    {
+                        currentDoubleQuotes = false;
+                        tokens.Add(Token.CLOSEDOUBLEQUOTE);
+                        continue;
+                    }
+                    tokens.Add(Token.OPENDOUBLEQUOTE);
+                    currentDoubleQuotes = true;
+                    continue;
+                }
+                if (c == '\'')
+                {
+                    if (currentSingleQuotes)
+                    {
+                        currentSingleQuotes = false;
+                        tokens.Add(Token.CLOSESINGLEQUOTE);
+                        continue;
+                    }
+                    tokens.Add(Token.OPENSINGLEQUOTE);
+                    currentSingleQuotes = true;
+                    continue;
+                }
+                if (c == '{')
+                {
+                    currentCurlies++;
+                    tokens.Add(Token.OPENCURLY);
+                    continue;
+                }
+                if (c ==  '}')
+                {
+                    currentCurlies--;
+                    tokens.Add(Token.CLOSECURLY);
+                    continue;
+                }
+                if (c == '[')
+                {
+                    currentSquares++;
+                    tokens.Add(Token.OPENSQUARE);
+                    continue;
+                }
+                if (c == ']')
+                {
+                    currentSquares--;
+                    tokens.Add(Token.CLOSESQUARE);
+                    continue;
+                }
+            }
+
+            if (totalOpenCurlies != totalCloseCurlies ||
+                totalOpenSquares != totalCloseSquares ||
+                totalOpenSingleQuotes != totalCloseSingleQuotes ||
+                totalOpenDoubleQuotes != totalCloseDoubleQuotes)
+            {
+                throw new Exception("invalid JSON string");
+            }
+
+            return tokens;
+        }
     }
 }
