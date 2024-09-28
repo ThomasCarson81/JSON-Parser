b786a3e93597ebbe20fd9210a4885dfe64caf764
Thomas Carson <thomas.carson2024@gmail.com>
2024-09-28 18:46:38 +0100

    Added Project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+
+namespace JsonParser
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
